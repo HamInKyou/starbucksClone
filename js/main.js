2cad6fd215bbe22fb86cf77da279da6f815a7935
HamInGyu <hfire1207@gmail.com>
2021-07-15 16:43:20 +0900

    [추가] visual Section의 요소들 에니메이션(안보이던것들 순차적으로 보이게)

--- js/main.js
+++ js/main.js
@@ -58,3 +58,11 @@ window.addEventListener(
     }
   }, 300)
 );
+
+const fadeEls = document.querySelectorAll(".visual .fade-in");
+fadeEls.forEach((fadeEl, index) => {
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * 0.7, //0.7, 1.4, 2.1, 2.7초 마다 한 요소씩 보여지게
+    opacity: 1,
+  });
+});
