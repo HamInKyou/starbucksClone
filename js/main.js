2da24eed2b5569f970a433829b3b809941356464
HamInGyu <hfire1207@gmail.com>
2021-07-19 15:41:38 +0900

    [추가] 유튜브섹션의 떠다니는 요소들 구현

--- js/main.js
+++ js/main.js
@@ -108,3 +108,24 @@ promotionToggleBtn.addEventListener("click", () => {
     promotionEl.classList.remove("hide");
   }
 });
+
+// 범위 랜덤 함수(소수점 2자리까지)
+function random(min, max) {
+  // `.toFixed()`를 통해 반환된 문자 데이터를,
+  // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+  return parseFloat((Math.random() * (max - min) + min).toFixed(2));
+}
+
+function floatingObject(selector, delay, size) {
+  gsap.to(selector, random(1.5, 2.5), {
+    y: size,
+    repeat: -1, //무한반복
+    yoyo: true, //한번재생했다가 다시 돌아갔다 다시 재생하는 구조
+    ease: Power1.easeInOut,
+    delay: random(0, delay), //지연시간
+  });
+}
+
+floatingObject(".floating1", 1, 15);
+floatingObject(".floating2", 0.5, 15);
+floatingObject(".floating3", 1.5, 20);
